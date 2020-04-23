7514e8d5e065af0c317d516ce222cc17d1721bd8
tanlingxiao <1025934610@qq.com>
2020-04-23 15:00:51 +0800

    add part6

---
+++ part6/迪杰斯特拉算法.py
@@ -0,0 +1,68 @@
+class Graph:
+    path = []
+
+    def minDistance(self, dist, queue):
+        minimum = float("Inf")
+        min_index = -1
+
+        for i in range(len(dist)):
+            if dist[i] < minimum and i in queue:
+                minimum = dist[i]
+                min_index = i
+        return min_index
+
+    def printPath(self, parent, j):
+        if parent[j] == -1:
+            # print(j)
+            self.path.append(j)
+            return
+        self.printPath(parent, parent[j])
+        # print(j)
+        self.path.append(j)
+
+    def printSolution(self, dist, parent):
+        src = 0
+        for i in range(1, len(dist)):
+            print("%d to %d distance is %d" % (src, i, dist[i]))
+            self.printPath(parent, i)
+            print('The path is: ', self.path)
+            self.path.clear()
+
+    def dijkstra(self, graph, src):
+        row = len(graph)
+        col = len(graph[0])
+        dist = [float("Inf")] * row
+        parent = [-1] * row
+        dist[src] = 0
+        queue = []
+        for i in range(row):
+            queue.append(i)
+
+        while queue:
+            u = self.minDistance(dist, queue)
+            queue.remove(u)
+            for i in range(col):
+                if graph[u][i] and i in queue:
+                    if dist[u] + graph[u][i] < dist[i]:
+                        dist[i] = dist[u] + graph[u][i]
+                        parent[i] = u
+
+        self.printSolution(dist, parent)
+
+
+if __name__ == '__main__':
+
+    g = Graph()
+
+    graph = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
+            [4, 0, 8, 0, 0, 0, 0, 11, 0],
+            [0, 8, 0, 7, 0, 4, 0, 0, 2],
+            [0, 0, 7, 0, 9, 14, 0, 0, 0],
+            [0, 0, 0, 9, 0, 10, 0, 0, 0],
+            [0, 0, 4, 14, 10, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 2, 0, 1, 6],
+            [8, 11, 0, 0, 0, 0, 1, 0, 7],
+            [0, 0, 2, 0, 0, 0, 6, 7, 0]
+            ]
+
+    g.dijkstra(graph, 0)
